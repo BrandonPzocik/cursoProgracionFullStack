633c4794e3142d10621b6d2eb829a51618e22714
BrandonPzocik <pzocikbrandon@gmail.com>
2025-02-11 20:24:13 -0300

    first commit

---
+++ part1/curso-react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Mensaje from './mensaje.jsx'
+const App = () => {
+ 
+  return (
+    <>
+      <div>
+       <Mensaje name= "hola, soy brandon"/>
+       <Mensaje name= "tengo 19 años"></Mensaje>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ part1/curso-react/src/mensaje.jsx
@@ -0,0 +1,5 @@
+const Mensaje = (props) => {
+    return <h1>{props.name}</h1>
+}
+
+export default Mensaje
